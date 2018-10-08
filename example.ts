f6050cd87cc6664f8044b07e32636f187160420d
lewix <>
2018-10-08 14:50:53 +0200

    update example file & timeout handled

--- example.ts
+++ example.ts
@@ -1,8 +1,28 @@
-import { MemoryCardManager } from './memorycard-manager'
+import { TsPcscLite } from './index';
 
+class Example {
+    static async main() {
 
+        console.log("Example started...");
+        let tsPcsc = TsPcscLite.instance;
+        console.log("Waiting for reader plugged...");
 
+        try {
 
-console.log("example started");
-MemoryCardManager.Uff();
-MemoryCardManager.WaitForReader();
+            let readerName : string = await tsPcsc.detectReader();
+            console.log(`Reader detected:${readerName}`);
+        }
+        catch (error) {
+            console.log(`Error!\n${error}`)
+        }
+        finally {
+
+            tsPcsc.close();
+            console.log("Example completed...");    
+        }
+        
+        console.log("Reader closed");
+    }
+}
+
+Example.main();
--- index.ts
+++ index.ts
@@ -4,15 +4,12 @@ export class TsPcscLite {
     private static _instance : TsPcscLite;
     private _pcsc : Pcsc;
 
+    private readonly _defaultTimeout;
+
     constructor() {
 
         this._pcsc = new Pcsc();
-    }
-
-    greeting: string;
- 
-    greet() {
-        return "Hello, " + this.greeting;
+        this._defaultTimeout = 15000;
     }
 
     static get instance () : TsPcscLite {
@@ -23,21 +20,22 @@ export class TsPcscLite {
         return this._instance;
     }
 
-    async detectReader() : Promise<string> {
+    async detectReader(timeout? : number) : Promise<string> {
 
+        if (timeout == null)
+            timeout = this._defaultTimeout;
 
-        // return new Promise<string>(resolve => {
-    
-        //     setTimeout(() => {
+        return new Promise<string>((resolve,reject) => {
 
-        //         resolve("ok detectReader");
-        //     }, 3000);
-            
-        // });
+            let cmdTimeout = setTimeout(() => {
 
-        return new Promise<string>(resolve => {
-            
+                this._pcsc.close();
+                reject(`Timeout: no card reader detected after ${timeout}ms`);
+            }, timeout);
+
             this._pcsc.on('reader', function(reader) {
+               
+                clearTimeout(cmdTimeout);
                 return resolve(reader.name);
             });
         });
@@ -46,4 +44,66 @@ export class TsPcscLite {
     close() : void {
         this._pcsc.close();
     }
+
+    // static RawDemo() : void {
+
+    //     let pcsc = new Pcsc();
+        
+    //     pcsc.on('reader', function(reader) {
+        
+    //         console.log('New reader detected', reader.name);
+        
+    //         reader.on('error', function(err) {
+    //             console.log('Error(', this.name, '):', err.message);
+    //         });
+        
+    //         reader.on('status', function(status) {
+    //             console.log('Status(', this.name, '):', status);
+    //             /* check what has changed */
+    //             var changes = this.state ^ status.state;
+    //             if (changes) {
+    //                 if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+    //                     console.log("card removed");/* card removed */
+    //                     reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+    //                         if (err) {
+    //                             console.log(err);
+    //                         } else {
+    //                             console.log('Disconnected');
+    //                         }
+    //                     });
+    //                 } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+    //                     console.log("card inserted");/* card inserted */
+    //                     reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+    //                         if (err) {
+    //                             console.log(err);
+    //                         } else {
+    //                             console.log('Protocol(', reader.name, '):', protocol);
+    //                             console.log('ATR: ', status.atr);
+                                
+                                
+                                
+    //                             reader.transmit(new Buffer([0xFF, 0xA4, 0x00, 0x00, 0x01, 0x06]), 40, protocol, function(err, data) {
+    //                                 if (err) {
+    //                                     console.log(err);
+    //                                 } else {
+    //                                     console.log('Data received', data);
+    //                                     reader.close();
+    //                                     pcsc.close();
+    //                                 }
+    //                             });
+    //                         }
+    //                     });
+    //                 }
+    //             }
+    //         });
+        
+    //         reader.on('end', function() {
+    //             console.log('Reader',  this.name, 'removed');
+    //         });
+    //     });
+        
+    //     pcsc.on('error', function(err) {
+    //         console.log('PCSC error', err.message);
+    //     });
+    // }
 }
